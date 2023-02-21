4f3c982bccba3d911b85c00951d41469d07aa1cf
Focrult <focrult@gmail.com>
2023-02-21 09:16:32 +0800

    Completed a draft version of the Product.js file

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,30 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: STRING,
+      allowNull: false,
+      isDecimal: true,
+    },
+    price: {
+      type: DECIMAL(10,2),
+      allowNull: false,
+      isDecimal: true,
+    },
+    stock: {
+      type: INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+    },
+    category_id: {
+      type: INTEGER,
+    }
   },
   {
     sequelize,
