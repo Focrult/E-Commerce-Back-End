e4fd0f69b1d6c46ed6f34ab0f893db91a3640246
Focrult <focrult@gmail.com>
2023-02-21 10:23:55 +0800

    Drafted tag-routes: Now going to implement the models product and product tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -5,7 +5,7 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 router.get('/', async (req, res) => {
   try{
-    const tagAll = await Category.findAll({include: [{model:Tag}], /*Eager Loading*/}); 
+    const tagAll = await Tag.findAll({include: [{model:Tag}], /*Eager Loading*/}); 
     res.status(200).json(tagAll);
   }catch(err){
   res.status(500);
@@ -15,20 +15,47 @@ router.get('/', async (req, res) => {
 });
 
 router.get('/:id', async (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+  try{
+    const tagAll = await Tag.findByPk(req.params.id, {include: [{model:Tag}],});
+    res.status(200).json(tagAll);
+  } catch(err){
+    res.status(500);
+  }finally{
+    console.log("Passing through router.get /:id");
+  }
 });
 
 router.post('/', async (req, res) => {
-  // create a new tag
+  try{
+    const tagAll = await Tag.create(req.body);
+    res.status(200).json(tagAll);
+  } catch(err){
+    res.status(500);
+  } finally{
+    console.log("Passing through reouter.post");
+  }
 });
 
 router.put('/:id', async (req, res) => {
-  // update a tag's name by its `id` value
+  try{
+    const tagAll = await Tag.update(req.body, {where: {id: req.params.id},}); 
+      res.status(200).json(tagAll);
+  } catch(err){
+    res.status(500);
+  } finally{
+    console.log("Passing through router.put");
+  }
 });
 
 router.delete('/:id', async (req, res) => {
-  // delete on tag by its `id` value
+  try{
+    const tagAll = await Tag.delete({where: {id: req.params.id}});
+    res.status(200).json(tagAll);
+  } catch(err){
+    res.status(500);
+  } finally{
+    console.log("Passing through router.delete");
+  }
 });
 
 module.exports = router;
