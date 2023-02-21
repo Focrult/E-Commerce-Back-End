c1070b9cf25f8522aa70167c5cce088851b6306b
Focrult <focrult@gmail.com>
2023-02-21 10:06:15 +0800

    drafted the category-routes.js file

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,9 +6,8 @@ const { Category, Product } = require('../../models');
 router.get('/', async (req, res) => {
   //Use try catch, finally.
   try{
-    const categoriesAll = await Category.findAll({
-    include: [{model:Product}], //Eager Loading
-  }); res.status(200).json(categoriesAll);
+    const categoriesAll = await Category.findAll({include: [{model:Product}], /*Eager Loading*/}); 
+    res.status(200).json(categoriesAll);
 }catch(err){
   res.status(500);
 }finally{
@@ -17,20 +16,47 @@ router.get('/', async (req, res) => {
 });
 
 router.get('/:id', async (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+  try{
+    const categoriesAll = await Category.findByPk(req.params.id, {include: [{model:Product}],});
+    res.status(200).json(categoriesAll);
+  } catch(err){
+    res.status(500);
+  }finally{
+    console.log("Passing through router.get /:id");
+  }
 });
 
 router.post('/', async (req, res) => {
-  // create a new category
+  try{
+    const categoriesAll = await Category.create(req.body);
+    res.status(200).json(categoriesAll);
+  } catch(err){
+    res.status(500);
+  } finally{
+    console.log("Passing through reouter.post");
+  }
 });
 
 router.put('/:id', async (req, res) => {
-  // update a category by its `id` value
+  try{
+    const categoriesAll = await Category.update(req.body, {where: {id: req.params.id},}); 
+      res.status(200).json(categoriesAll);
+  } catch(err){
+    res.status(500);
+  } finally{
+    console.log("Passing through router.put");
+  }
 });
 
 router.delete('/:id', async (req, res) => {
-  // delete a category by its `id` value
+  try{
+    const categoriesAll = await Category.delete({where: {id: req.params.id}});
+    res.status(200).json(categoriesAll);
+  } catch(err){
+    res.status(500);
+  } finally{
+    console.log("Passing through router.delete");
+  }
 });
 
 module.exports = router;
